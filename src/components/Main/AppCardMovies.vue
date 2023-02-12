c68c4776de74f9be3db798c686cd762de8583dfe
Davide Di Girolamo <davide.digi2001@gmail.com>
2023-02-12 16:04:02 +0100

    completata prima parte milestone 3

--- src/components/Main/AppCardMovies.vue
+++ src/components/Main/AppCardMovies.vue
@@ -45,6 +45,14 @@ export default {
             }
             return flag;
 
+        },
+        urlsAssemble(urlEnd) {
+            const urlStart = 'https://image.tmdb.org/t/p/';
+            const urlWidth = 'w342';
+
+            let Alltogether = urlStart + urlWidth + urlEnd;
+
+            return Alltogether;
         }
 
     }
@@ -57,8 +65,9 @@ export default {
     <div class="text-white d-flex" v-for="movie in store.movies">
         <h5><em>Titolo Film: </em>{{ movie.title }} |</h5>
         <h6><em> Titolo originale: </em>{{ movie.original_title }} |</h6>
+        <img class="poster" :src="urlsAssemble(movie.poster_path)" :alt="movie.title">
         <p class="ms-2"><em> Lingua: </em>{{ movie.original_language }} </p>
-        <img class="ms-2" :src="generateFlag(movie.original_language)" alt="ooo">
+        <img class="ms-2 flag" :src="generateFlag(movie.original_language)" alt="ooo">
         <div class="ms-3">|<em> Voto: </em>{{ movie.vote_average }}</div>
         <hr>
     </div>
@@ -66,8 +75,9 @@ export default {
     <div class="text-white d-flex" v-for="serie in store.series">
         <h5><em>Titolo Serie-TV: </em>{{ serie.name }} |</h5>
         <h6><em> Titolo originale: </em>{{ serie.original_name }} |</h6>
+        <img class="poster" :src="urlsAssemble(serie.poster_path)" :alt="serie.name">
         <p class="ms-2"><em> Lingua: </em>{{ serie.original_language }} </p>
-        <img class="ms-2" :src="generateFlag(serie.original_language)" alt="ooo">
+        <img class="ms-2 flag" :src="generateFlag(serie.original_language)" alt="ooo">
         <div class="ms-3">|<em> Voto: </em>{{ serie.vote_average }}</div>
     </div>
 
@@ -78,9 +88,15 @@ em {
     color: orange;
 }
 
-img {
+.flag {
     width: 20px;
     height: 20px;
+    object-fit: contain;
+}
+
+.poster {
+    width: 200px;
+    height: 200px;
     object-fit: contain;
 }
 </style>
